e3c300471e5d6e46ae837be1cff4051201fafe15
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-05 16:10:07 +0200

    Commit, esercizio2 work in progress

--- src/Esercizio2_Package/Esercizio2_Main.java
+++ src/Esercizio2_Package/Esercizio2_Main.java
@@ -1,5 +1,20 @@
 package Esercizio2_Package;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Esercizio2_Main {
 
+	public static void main(String[] args) {
+
+		List<Integer> numbersArray = new ArrayList<>();
+
+		Random randomNumber = new Random();
+
+		for (int i = 0; i < 3000; i++) {
+			numbersArray.add(randomNumber.nextInt());
+		}
+
+	}
 }
